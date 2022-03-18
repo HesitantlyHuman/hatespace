9ce3137bbf8ff40abae12a96c7f298e1c6dabc38
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2022-03-18 14:08:23 -0600

    Fix Trainer name

--- hatespace/training/trainer.py
+++ hatespace/training/trainer.py
@@ -1,15 +1,15 @@
 from transformers import Trainer
 from torch.utils.data import DataLoader
-from hatespace.datasets.base import DataLoader as IronmarchDataLoader
+from hatespace.datasets.base import DataLoader as HatespaceDataLoader
 
 # TODO: We may want to modify this class so that we can easily switch
 # between using sinkhorn, kl, vertex loss and others.
-class IronmarchTrainer(Trainer):
+class HatespaceTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         raise NotImplementedError
 
     def get_train_dataloader(self) -> DataLoader:
-        return IronmarchDataLoader(
+        return HatespaceDataLoader(
             self.train_dataset,
             batch_size=self.args.train_batch_size,
             num_workers=self.args.dataloader_num_workers,
@@ -17,7 +17,7 @@ class IronmarchTrainer(Trainer):
         )
 
     def get_eval_dataloader(self) -> DataLoader:
-        return IronmarchDataLoader(
+        return HatespaceDataLoader(
             self.eval_dataset,
             batch_size=self.args.eval_batch_size,
             num_workers=self.args.dataloader_num_workers,
