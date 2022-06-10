b6b105faf6741db7199da72b74cc179aafa49ba3
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2022-06-09 20:43:20 -0600

    Remove unnecessary example scripts

--- examples/datasets.py
+++
@@ -1,9 +0,0 @@
-from hatespace.datasets import IronMarch
-from hatespace.models.nlp import Tokenizer
-
-dataset = IronMarch("iron_march_201911")
-dataset.summary()
-
-tokenizer = Tokenizer("roberta-base", 512)
-dataset = dataset.map(tokenizer, batch_size=256)
-dataset.summary()
--- examples/models.py
+++
@@ -1,3 +0,0 @@
-from hatespace.models.nlp import ArchetypalTransformer
-
-model = ArchetypalTransformer("roberta-base", num_archetypes=12)
