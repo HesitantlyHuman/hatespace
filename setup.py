0fe3ad27a96ab9fc6f12feed5d4f318a3d74582c
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2022-06-15 14:42:48 -0600

    Update setup to include additional installation requirements

--- setup.py
+++ setup.py
@@ -7,16 +7,19 @@ README_FILE = open(os.path.join(PACKAGE_ROOT, "README.md"), "r").read()
 if __name__ == "__main__":
     setup(
         name="hatespace",
-        version="0.0.0",
+        version="0.1.0",
         description="Novel Archetypal Analysis NLP on hateful text corpora",
         long_description=README_FILE,
         long_description_content_type="text/markdown",
-        url="https://github.com/GenerallyIntelligent/hatespace",
-        author="GenerallyIntelligent",
+        url="https://github.com/HesitantlyHuman/hatespace",
+        author="HesitantlyHuman",
         author_email="tannersims@generallyintelligent.me",
         license="MIT",
         packages=find_packages(),
         install_requires=[
             "torch >=1.10.0, <2.0.0",
+            "geomloss",
+            "html2text >=2020.0.0",
+            "transformers >=4.0.0",
         ],
     )
