a7e1d75748ca74c1828109a50b30a2818a9c6faf
LamNguyenNN <lamnguyen1029@hotmail.com>
2022-07-13 14:56:28 -0400

    Added analysis suite of functions. Updated dirichlet_gof to something actually working

---
+++ hatespace/analysis/analysis_suite.py
@@ -0,0 +1,245 @@
+import numpy as np
+import pandas as pd
+import random
+from sklearn.datasets import make_classification
+from sklearn.neighbors import NearestNeighbors
+from scipy.optimize import lsq_linear, linear_sum_assignment
+from scipy.spatial import distance_matrix
+from scipy.special import psi, polygamma, betainc, gamma
+import ast
+import matplotlib.pyplot as plt
+from scipy import special
+import time
+import logging
+import seaborn as sns
+import threading
+from typing import Optional, Tuple, Union
+from hatespace.datasets import IronMarch
+import re
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import text
+from hatespace.analysis import DirichletGOF
+import os
+from datetime import datetime
+
+
+class IronmarchAnalysis:
+
+	def __init__(self,
+		dataset_path: str, 
+		path_to_latent_vector_file: Optional[str] = '', 
+		latent_vectors: Optional[np.ndarray] = None
+	):
+
+		if latent_vectors is not None:
+			self.latent_vectors = latent_vectors
+		elif path_to_latent_vector_file is not '':
+			try:
+				self.latent_vectors = np.load(path_to_latent_vector_file)
+			except:
+				print('Latent vectors file must by .npy file')
+		else:
+			raise ValueError('Must provide path to a latent vectors file, or provide a latent vectors array directly')
+
+		dataset = IronMarch(dataset_path)
+		self.msgs = pd.read_csv(os.path.join(dataset_path, 'core_message_posts.csv'))
+		self.forums = pd.read_csv(os.path.join(dataset_path, 'core_search_index.csv'))
+
+		self.posts = [x['data'] for x in dataset]
+		self.posts = [str(i) for i in texts]
+		
+		self.post_ids = [x['id'] for x in dataset] 
+
+		self.id_index_dict = dict((value, idx) for idx,value in enumerate(self.post_ids))
+
+
+
+	def get(self, 
+		unix_start_time: Optional[int] = None, 
+		unix_end_time: Optional[int] = None,
+		start_ymd: Optional[tuple] = None, 
+		end_ymd: Optional[tuple] = None, 
+		author_ids: Optional[list] = [],
+		split_by = ''
+		):
+		
+
+		if unix_start_time is not None:
+			start = unix_start_time
+		elif start_ymd is not None:
+			start = int(datetime(start_ymd[0], start_ymd[1], start_ymd[2]).timestamp())
+		else:
+			start = -1
+
+		if unix_end_time is not None:
+			end = unix_end_time
+		elif end_ymd is not None:
+			end = int(datetime(end_ymd[0], end_ymd[1], end_ymd[2]).timestamp())
+		else:
+			end = 1e99
+
+
+		ranged_forums = self.forums[(self.forums['index_date_created'] > start) & (self.forums['index_date_created'] < end)]
+		ranged_msgs = self.msgs[(self.msgs['msg_date'] > start) & (self.msgs['msg_date'] < end)]
+
+		if len(author_ids) > 0:
+			ranged_forums = ranged_forums[ranged_forums['index_author'].isin(author_ids)]
+			ranged_msgs = ranged_msgs[ranged_msgs['msg_author_id'].isin(author_ids)]
+
+		forums_ids = ['forum_post-' + str(x) for x in ranged_forums['index_id'].tolist()]
+		msgs_ids = ['direct_message-' + str(x) for x in ranged_msgs['msg_id'].tolist()]
+
+		ranged_ids = forums_ids + msgs_ids
+		timestamps = ranged_forums['index_date_created'].tolist() + ranged_msgs['msg_date'].tolist()
+
+		indices = [self.id_index_dict[x] for x in ranged_ids]
+
+		ranged_latent_vectors = self.latent_vectors[indices]
+
+		sort_indices = np.argsort(timestamps)
+		ranged_latent_vectors = ranged_latent_vectors[sort_indices]
+		timestamps = [timestamps[i] for i in sort_indices]
+		ranged_ids = [ranged_ids[i] for i in sort_indices]
+
+		posts = get_posts_from_post_ids(ranged_ids)
+
+		if split_by != '':
+			if split_by.lower() == 'day':
+				dates = [datetime.fromtimestamp(x).strftime('%d/%m/%Y') for x in timestamps]
+			elif split_by.lower() == 'month':
+				dates = [datetime.fromtimestamp(x).strftime('%m/%Y') for x in timestamps]
+			current = None
+			split_indices = []
+
+			for idx, date in enumerate(dates):
+				if current != date:
+					current = date
+					time_indices = []
+					split_indices.append(time_indices)
+				time_indices.append(idx)
+
+			split_dict = []
+			for split in split_indices:
+				split_latent_vectors = latent_vectors[split]
+				split_timestamps = [timestamps[i] for i in split]
+				split_ids = [ranged_ids[i] for i in split]
+				split_posts = get_posts_from_post_ids(split_ids)
+
+				split_dict.append({'latent_vectors': split_latent_vectors, 'ids': split_ids, 'timestamps': split_timestamps, 'posts': split_posts})
+
+			return split_dict
+
+		return {'latent_vectors': ranged_latent_vectors,'sorted_ids': ranged_ids, 'sorted_timestamps': timestamps, 'sorted_posts': posts}
+
+
+	def get_nearest_indices(self, num_vectors_per_at: int) -> np.ndarray:
+		'''
+		Returns array of indices closest to all archetypes.
+
+		'''
+
+		# Computes distances from latent vectors to every corner of simplex
+		latent_dim_size = self.latent_vectors.shape[1]
+		dists = np.zeros((latent_dim_size, self.latent_vectors.shape[0]))
+		for i, vertex in enumerate(np.eye(latent_dim_size)):
+	  		dists[i] = np.sqrt(np.sum(np.square(self.latent_vectors-vertex), axis=1))
+
+	  	# Get closest indices to each archetype by sorting
+	  	nearest_indices = np.argsort(dists)[:, :num_vectors_per_at]
+
+	  	return nearest_indices
+
+
+	# Gets specified number of archetypal posts
+	# Only returns list of lists containing the posts. Update to have pandas option
+	def get_archetypal_posts(self, num_posts_per_at: int):
+		nearest_indices = self.get_nearest_indices(self.latent_vectors, num_posts_per_at)
+		latent_dim_size = self.latent_vectors.shape[1]
+		at_posts = []
+		for i in range(latent_dim_size):
+		    top_posts = [texts[k] for k in nearest_indices[i]]
+		    at_posts.append(top_posts)
+		return at_posts
+
+
+	# Keyword extraction using TF-IDF algorithm. Return as pandas array
+	# TODO: Provide a visualization that sorts TF-IDF scores for each archetype and makes a bar plot
+	def archetypes_tfidf_scores(self, num_posts_per_at: int):
+		# For each archetype, concatenate all posts into one string.
+		# We will take all the posts in a single archetype to be one document
+		nearest_indices = self.get_nearest_indices(self.latent_vectors, num_posts_per_at)
+		latent_dim_size = self.latent_vectors.shape[1]
+		at_posts = []
+		for i in range(latent_dim_size):
+			top_posts = [texts[k] for k in nearest_indices[i]]
+			post = ''
+		  	for j in range(20):
+		    	top = top_posts[j].lower(0)
+		    	top = re.sub(r"<url>", ' ', top) # Can probably combine these two re lines
+		    	top = re.sub('[\n\t\r]', ' ', top)
+		    	post += top
+		    	post += ''
+		  	at_posts.append(post)
+
+		my_stop_words = text.ENGLISH_STOP_WORDS
+		vectorizer = TfidfVectorizer(stop_words=my_stop_words)
+		vectors = vectorizer.fit_transform(combined_at_posts)
+
+		feature_names = vectorizer.get_feature_names()
+		dense = vectors.todense()
+		denselist = dense.tolist()
+		df = pd.DataFrame(denselist, columns=feature_names)
+
+		return df
+
+
+	# TODO: Some way to obtain weights and biases of side information linear head
+	def get_weights_and_biases():
+		pass
+		
+
+	# Visualizes pairwise angles between hyperplane normal vectors obtained from linear head
+	def compute_normal_hyperplanes(self, weights):
+		class_names = ['nationality', 'ethnicity', 'religion', 'gender', 'sexual_orientation', 'disability', 'class']
+		
+		weights_norm = weights / np.linalg.norm(weights, axis=1)[:, np.newaxis]
+
+		dot_prods = np.zeros((7,7))
+		for i in range(7):
+		  for j in range(7):
+		    dot_prods[i][j] = np.dot(weights_norm[i], weights_norm[j])
+
+		sns.heatmap(dot_prods, cmap=sns.cm.rocket_r, xticklabels=class_names, yticklabels=class_names)
+
+
+	# Get proportion of posts that lie on either side of hyperplane generated by side info
+	def compute_hyperplane_proportions(self, weights, biases):
+		proportions = []
+		for i in range(7):
+			count = 0
+			for j in range(self.latent_vectors.shape[0]):
+			if np.dot(weights[i], self.latent_vectors[j]) + biases[i] > 0:
+				count += 1
+			proportions.append(count / self.latent_vectors.shape[0])
+
+		print('Proportion of posts above each hyperplane')
+		for idx, i in enumerate(class_names):
+			print(class_names[idx] + ': ' + str(proportions[idx]))
+
+
+	# TODO: implement full dirichlet analysis of latent space
+	def dirichlet_analysis(self, significance_level, dim, sample_size, crit_val_arr=None):
+		gof = DirichletGOF(significance_level, dim, sample_size, crit_val_arr=None)
+
+		results = gof.test_statistic(self.latent_vectors)
+
+		return results
+
+
+	'''
+	track dirichlet-ness over time
+	track centroid movement over time (all users/certain bad actors)
+	use kernel density estimator to make plot along each archetypal direction (seaborn)
+	csv of embedded coordinates, time, user id, location - maybe average each coordinate according to location
+	'''
--- hatespace/analysis/dirichlet_tools/dirichlet_gof.py
+++ hatespace/analysis/dirichlet_tools/dirichlet_gof.py
@@ -6,34 +6,71 @@ from scipy.optimize import lsq_linear
 from scipy.special import psi, polygamma, betainc, gamma
 
 class DirichletGOF:
-    def __init__(self, significance_level, dim, sample_size, num_iter, energy_dist_pow=1):
+    def __init__(self, significance_level, dim, sample_size, crit_val_arr=None):
         self.significance_level = significance_level
         self.sample_size = sample_size
-        self.num_iter = num_iter
         self.dim = dim
-        self.energy_dist_pow = energy_dist_pow
+  
+        self.dir_dist = self.dirichlet_dist(self.dim)
 
-        self.dir_dist = self.avg_dirichlet_distance()
-        self.crit_value = self.critical_value()
+        self.Dir = self.symm_dirichlet(1)
 
-    def mle_alpha(self, x, max_iter=200):
-        '''Given a set of points x, compute minimum likelihood estimation of Dirichlet concentration parameters '''
+        if crit_val_arr is None:
+            self.crit_value = self.critical_value()
+        else:
+            self.crit_value = np.quantile(crit_val_arr, self.significance_level)
+
+    class symm_dirichlet:
+        def __init__(self, alpha, resolution=2**16):
+            self.alpha = alpha
+            self.resolution = resolution
+            self.range, delta = np.linspace(0, 1, resolution,
+                                            endpoint=False, retstep=True)
+            self.range += delta / 2
+            self.table = special.gammaincinv(self.alpha, self.range)
+
+        def draw(self, n_sampl, n_comp, interp='nearest'):
+            if interp != 'nearest':
+                raise NotImplementedError
+            gamma = self.table[np.random.randint(0, self.resolution, (n_sampl, n_comp))]
+            return gamma / gamma.sum(axis=1, keepdims=True)
+        
+    def initial_params(self, x):
+        x1 = np.mean(x, axis=0)
+        x2 = np.mean(x[:, 0]**2)
+
+        alpha_init = x1 * (x1[0] - x2) / (x2 - x1[0]**2)
+
+        return alpha_init
+
+    def trigamma(self, x):
+        return polygamma(1, x)
+
+    def mle_alpha(self, x, max_iter=150, eps=1e-7):
         n = x.shape[0]
-        alpha = initial_params(x)
+        alpha = self.initial_params(x)
         prev = alpha
 
         for idx in range(max_iter):
-        grad = n * (psi(np.sum(alpha)) - psi(alpha)) + np.sum(np.log(x), axis=0)
-        q = -n * trigamma(alpha)
-        c = n * trigamma(np.sum(alpha))
-        b = np.sum(grad/q) / ((1/c) + np.sum(1/q))
-        alpha = alpha - (grad - b)/q
+            grad = n * (psi(np.sum(alpha)) - psi(alpha)) + np.sum(np.log(x), axis=0)
 
-        if np.any(alpha<0):
-          return prev
-        else:
-          prev = alpha
+            q = -n * self.trigamma(alpha)
+            c = n * self.trigamma(np.sum(alpha))
+            b = np.sum(grad/q) / ((1/c) + np.sum(1/q))
 
+            alpha = alpha - (grad - b)/q
+
+            displacement = alpha - prev
+            dist = np.sqrt(np.einsum('i,i', displacement, displacement))
+
+            if np.any(alpha<0):
+                return prev
+
+            if dist < eps:
+                break
+            else:
+                prev = alpha
+          
         return alpha
 
     def dirichlet_transform(self, x, alpha):
@@ -45,81 +82,108 @@ class DirichletGOF:
         n = x.shape[1]-1
 
         for i in range(num_samples):
-        up_lim = x[i][0]
-        y[i][0] = betainc(alpha[0], np.sum(alpha[1:]), up_lim)
-        x_[i][0] = 1-np.power(1-y[i][0], 1/n)
+            up_lim = x[i][0]
+            y[i][0] = betainc(alpha[0], np.sum(alpha[1:]), up_lim)
+            x_[i][0] = 1-np.power(1-y[i][0], 1/n)
 
-        for j in range(1, n):
-          remainder = 1 - np.sum(x[i][:j])
-          if remainder == 0:
-            up_lim = np.float64(1)
-          else:
-            up_lim = x[i][j] / remainder
+            for j in range(1, n):
+                remainder = 1 - np.sum(x[i][:j])
+                if remainder == 0:
+                    up_lim = np.float64(1)
+                else:
+                    up_lim = x[i][j] / remainder
 
-          y[i][j] = betainc(alpha[j], np.sum(alpha[j+1:]), up_lim.clip(min=0, max=1))
-          x_[i][j] = np.prod([np.power(1-y[i][k], 1/(n-k)) for k in range(j)]) * (1-np.power(1-y[i][j], 1/(n-j)))
+                y[i][j] = betainc(alpha[j], np.sum(alpha[j+1:]), up_lim.clip(min=0, max=1))
+                x_[i][j] = np.prod([np.power(1-y[i][k], 1/(n-k)) for k in range(j)]) * (1-np.power(1-y[i][j], 1/(n-j)))
 
         x_[:, -1] = 1 - np.sum(x_[:, :-1], axis=1)
 
         return x_
 
-    def avg_dirichlet_distance(self, num_samples=1e6):
+    def dirichlet_dist(self, alpha_dim, num_samples=1000000):
         '''Simulates E|Z_1 - Z_2| where Z_1 and Z_2 are standard Dirichlet distributed variables'''
-        z1 = np.random.dirichlet(alpha=[1]*self.dim, size=num_samples)
-        z2 = np.random.dirichlet(alpha=[1]*self.dim, size=num_samples)
-        dirichlet_dist = np.mean(np.linalg.norm(z1-z2, axis=1))
+        z1 = np.random.dirichlet(alpha=[1.]*alpha_dim, size=num_samples)
+        z2 = np.random.dirichlet(alpha=[1.]*alpha_dim, size=num_samples)
+        dirichlet_dist = np.mean(np.sqrt(np.einsum('ij,ij->i', z1-z2, z1-z2)))
+
         return dirichlet_dist
 
-    def critical_value(self):
-        '''Compute critical value of test depending on significance level'''
-        x = np.random.dirichlet(alpha=[1]*self.dim, size=self.sample_size)
-        alpha_mle = self.mle_alpha(x)
-        x = self.dirichlet_transform(x, alpha_mle)
 
-        tests = []
-        for i in range(20000):
-            z = np.random.dirichlet(alpha=[1]*self.dim, size=self.num_samples)
-            tests.append(np.sum(np.mean(np.linalg.norm(x[:, None, :] - z[None, :, :], axis=-1), axis=1)))
+    def critical_value(self, num_samples=20000, num_threads=4):
+        x_samples = np.random.dirichlet([1]*self.dim, (num_samples, self.sample_size))
 
-        sample_dirichlet_dist = np.quantile(tests, q=self.significance_level)
-        sample_dists = np.sum(np.power(np.linalg.norm(x[:, None, :] - x[None, :, :], axis=-1), self.energy_dist_pow))
-        statistic = sample_size * ( ((2/sample_size) * sample_dirichlet_dist) - self.dir_dist - ((1/sample_size**2) * sample_dists))
+        threads = list()
+        subdiv = int(num_samples / num_threads)
 
-        return statistic
+        statistics_arr = np.zeros((num_samples,))
+
+        def compute_statistics(samples, start):
+            for i in range(subdiv):
+                z = self.Dir.draw(20000, self.dim)
+                alpha_mle = self.mle_alpha(samples[i])
+                samples[i] = self.dirichlet_transform(samples[i], alpha_mle)
+
+                x = samples[i]
+                x_ = x[:, None, :]
+                sample_dist = np.sum(np.sqrt(np.einsum('ijk, ijk->ij', x-x_, x-x_)))
+                sample_dirichlet_dist = np.sum(np.mean(np.sqrt(np.einsum('ijk->ij',(x_ - z)**2)), axis=1))
+                statistic = (2 * sample_dirichlet_dist) - (self.sample_size * self.dir_dist) - ((1/self.sample_size) * sample_dist)
+
+                statistics_arr[start+i] = statistic
+     
+        for index in range(num_threads):
+            x = threading.Thread(target=compute_statistics, args=(x_samples[index * subdiv: (index+1)*subdiv], index * subdiv,))
+            threads.append(x)
+            x.start()
+
+        for thread in threads:
+            thread.join()
+
+        statistics_arr = statistics_arr[~np.isnan(statistics_arr)]
+        crit_val = np.quantile(statistics_arr, self.significance_level)
+
+        return crit_val, statistics_arr
 
     def energy_statistic(self, x):
-        '''Compute test statistic of a given sample x'''
-        sample_size = x.shape[0]
+        input_size = x.shape[0]
         n = x.shape[1]
+      
+        x_ = x[:, None, :]
 
-        tests = []
-        for i in range(20000):
-        z = np.random.dirichlet(alpha=[1]*self.dim, size=n)
-        tests.append(np.sum(np.mean(np.linalg.norm(x[:, None, :] - z[None, :, :], axis=-1), axis=1)))
+        input_dist = np.sum(np.sqrt(np.einsum('ijk, ijk->ij', x-x_, x-x_)))
 
-        sample_dirichlet_dist = np.mean(tests)
-        sample_dists = np.sum(np.power(np.linalg.norm(x[:, None, :] - x[None, :, :], axis=-1), d))
-        statistic = sample_size * ( ((2/sample_size) * sample_dirichlet_dist) - self.dir_dist - ((1/sample_size**2) * sample_dists))
+        input_dirichlet_dist = np.zeros((input_size, ))
+        for i in range(self.sample_size):
+            z = self.Dir.draw(20000, n)
+            sub = x[i] - z
+            input_dirichlet_dist[i] = np.mean(np.sqrt(np.einsum('ij,ij->i', sub, sub)))
+
+        input_dirichlet_dist = np.sum(input_dirichlet_dist)
+
+        statistic = (2 * input_dirichlet_dist) - (input_size * self.dir_dist) - ((1/input_size) * input_dist)
 
         return statistic
 
-    def compute_power(self, x, log=False):
-        '''Compute power of Dirichlet GOF test'''
+    def energy_distance(self, x, dir_dist, d=1):
+        return energy_statistic(x, dir_dist, d) / x.shape[0]
 
-        if log:
-            print('Using critical value at %.2f level of significance = %.4f' % (self.significance_level, self.crit_value))
+    def test_statistic(self, x, n_iter=1000, print_log=False):
+        print('Critical value:', self.crit_value)
 
-        test_stats_list = []
+        random_indices = np.random.rand(n_iter, x.shape[0]).argpartition(self.sample_size, axis=1)[:,:self.sample_size]
+        stats = []
         for i in range(n_iter):
-            x_sample = x[np.random.choice(np.linspace(0, x.shape[0]-1, x.shape[0]).astype(np.int32), size=sample_size, replace=False)]
-            alpha = self.mle_alpha(x_sample)
-            x_sample_transform = self.dirichlet_transform(x_sample, alpha)
+            x_sample = x[random_indices[i]] # optimized
+            alpha = self.mle_alpha(x_sample) # optimized
+            x_sample_transform = self.dirichlet_transform(x_sample, alpha) # optimized
 
-            test_stat = self.energy_statistic(x_sample_transform, self.dir_dist)
-            test_stats_list.append(test_stat)
+            test_stat = self.energy_statistic(x_sample_transform)
+            stats.append(test_stat)
             if print_log:
-              print('Finished iteration', str(i))
-              print('Test statistic:', test_stat)
-              print()
+                print('Finished iteration', str(i))
+                print('Test statistic:', test_stat)
+                #print('Test distance:', test_stat / x_sample_transform.shape[0])
+                print()
 
-        return test_stats_list, sum(i > self.crit_value for i in stats) / n_iter
+        return {'Sample Test Statistics': stats, 'Power': sum(i > crit_val for i in stats) / n_iter}
+
