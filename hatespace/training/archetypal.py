37e728f105bc2ce5098aaefca93cfc2a12763b3c
LamNguyenNN <lamnguyen1029@hotmail.com>
2022-10-13 16:32:29 -0600

    Added side information output to archetypal trainer

--- hatespace/training/archetypal.py
+++ hatespace/training/archetypal.py
@@ -16,22 +16,24 @@ class ArchetypalTrainer(HatespaceTrainer):
             attention_mask=attention_mask,
             decoder_attention_mask=attention_mask,
         )
-        predicted_sequence_logits, embeddings = (
+        predicted_sequence_logits, embeddings, feature_predictions = (
             model_outputs.logits,
             model_outputs.embeddings,
+            model_outputs.feature_predictions,
         )
         del model_outputs
 
-        return predicted_sequence_logits, embeddings
+        return predicted_sequence_logits, embeddings, feature_predictions
 
     def calculate_loss(
         self,
         tokens: Dict[str, torch.Tensor],
     ) -> torch.Tensor:
-        model_predictions, embeddings = self.batch_prediction(tokens=tokens)
+        model_predictions, embeddings, feature_predictions = self.batch_prediction(tokens=tokens)
         loss = self.loss_function(
             model_predictions,
             tokens["input_ids"],
             embeddings,
+            feature_predictions,
         )
         return loss
