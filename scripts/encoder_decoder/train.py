771202b32242c3130af48fc80e3ef855121610f1
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2023-05-01 11:43:50 -0600

    Update encoder decoder training script to allow loading pretrained weights from previous runs

--- scripts/encoder_decoder/train.py
+++ scripts/encoder_decoder/train.py
@@ -17,8 +17,11 @@ args.add_argument("--max_learning_rate", type=float, default=3e-5)
 args.add_argument("--weight_decay", type=float, default=0.01)
 args.add_argument("--training_steps", type=int, default=300_000)
 args.add_argument("--warmup_proportion", type=int, default=0.1)
+args.add_argument("--dataset", type=str, default="cc_news")
 args.add_argument("--data_root", type=str, default="data/cc_news")
 args.add_argument("--save_path", type=str, default="checkpoints/encoder_decoder")
+args.add_argument("--base_model", type=str, default="roberta-base")
+args.add_argument("--pretrained_model_path", type=str, default=None)
 args.add_argument("--save_every", type=int, default=750)
 args.add_argument("--quantile_clip", type=float, default=0.7)
 args.add_argument("--minibatch_size", type=int, default=MAX_SINGLE_BATCH_SIZE)
@@ -31,11 +34,18 @@ args.add_argument(
 
 config = vars(args.parse_args())
 
-model_name = "roberta-base"
+model_name = config["base_model"]
 
 print("Loading transformer models...")
 tokenizer = Tokenizer(model_name, 512)
 model = EncoderDecoderModel.from_encoder_decoder_pretrained(model_name, model_name)
+if config["pretrained_model_path"] is not None:
+    state_dict = torch.load(config["pretrained_model_path"])
+    try:
+        model.load_state_dict(state_dict)
+    except RuntimeError:
+        state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+        model.load_state_dict(state_dict)
 if torch.cuda.is_available():
     DEVICE = "cuda"
     # if torch.cuda.device_count() > 1:
@@ -47,7 +57,7 @@ else:
 model.to(DEVICE)
 
 train_loader, val_loader = prepare_dataloaders(
-    "cc_news",
+    config["dataset"],
     training_batch_size=config["batch_size"],
     validation_batch_size=config["batch_size"],
     num_workers=12,
@@ -85,13 +95,14 @@ trainer = EncoderDecoderTrainer(
     loss_function=loss_fn,
     epochs=num_epochs,
     minibatch_size=config["minibatch_size"],
+    validation_minibatch_size=config["minibatch_size"],
     configuration=config,
 )
 
 # We may need to load the dataset again if the seed has changed
 print("Loading dataset...")
 train_loader, val_loader = prepare_dataloaders(
-    "cc_news",
+    config["dataset"],
     training_batch_size=trainer.config["batch_size"],
     validation_batch_size=trainer.config["batch_size"],
     num_workers=12,
